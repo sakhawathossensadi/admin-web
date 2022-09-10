a789df139ca68f9ecc643e2cf88e1169a147e97a
sakhawathossensadi <newtonss919@gmail.com>
2022-09-10 23:49:30 +0600

    add Dashboard to route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Registration from './page/Registration/Registration';
 import Login from './page/Login/Login';
+import Dashboard from './page/Dashboard/Dashboard';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route index element={<Registration />} />
         <Route path="/login" element={<Login />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
     </Routes>
   </BrowserRouter>
   );
